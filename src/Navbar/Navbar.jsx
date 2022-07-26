f2a893f6402f024950711d93be3b4a42a2b9dd74
Ezequiel Gómez <ezefgomez94@gmail.com>
2022-07-26 00:07:28 -0300

    Mejora en navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import carrito from './carrito.png'
 
+
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-            <div style={{color: "black", width: "500px"}}>
-                <h1>PapaBear supply</h1>
-            </div>
             <div className="navbar-collapse">
-                <div className="navbar-nav" style={{width: "200px"}}>
-                    <button style={{backgroundColor:"grey"}}>Nuestros Productos</button>
+                <h1 style={{color: "black", width: "300px", display: "inline-block"}}>PapaBear supply</h1>
+                <div className="navbar-nav" style={{width: "300px",  display: "inline-block"}}>
+                    <button type="button" style={{backgroundColor:"grey",  display: "inline-block"}}>Nuestros Productos</button>
                 </div>
-                <div className="navbar-nav" style={{color: "white", width: "200px"}}>
-                    <button style={{backgroundColor:"grey"}}>Sobre Nosotros</button>
+                <div className="navbar-nav" style={{color: "white", width: "300px",  display: "inline-block"}}>
+                    <button type="button" style={{backgroundColor:"grey"}}>Sobre Nosotros</button>
                 </div>
-                <div className="navbar-nav" style={{color: "white", width: "200px"}}>
-                    <button style={{backgroundColor:"grey"}}>Contactanos</button>
+                <div className="navbar-nav" style={{color: "white", width: "300px",  display: "inline-block"}}>
+                    <button type="button" style={{backgroundColor:"grey"}}>Contactanos</button>
                 </div>
-                <div className="navbar-nav" style={{color: "white", width: "200px"}}>
-                    <button style={{backgroundColor:"grey"}}><img src={carrito} alt="" style={{width: "100px", height: "60px"}} /></button>
+                <div className="navbar-nav" style={{color: "white", width: "100px",  display: "inline-block"}}>
+                    <button type="button" style={{backgroundColor:"grey"}}><img src={carrito} alt="" style={{width: "100px", height: "60px"}} /></button>
                 </div>
             </div>
         </nav>
